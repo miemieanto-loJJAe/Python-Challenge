9775f8c6e475ad48e976ab6807fee507c756a7d3
miemieanto-loJJAe <miemie.anto@gmail.com>
2023-05-03 22:11:32 -0400

    Testing

---
+++ PyBank/main.py
@@ -0,0 +1,13 @@
+import os
+
+import csv
+
+csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
+
+with open(csvpath) as csvfile:
+
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    print(csvreader)
+
+    csv_header = next(csvreader)
