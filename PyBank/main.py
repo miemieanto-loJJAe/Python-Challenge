0a230f62e2f14486828cbffe056efe4de182eac4
miemieanto-loJJAe <miemie.anto@gmail.com>
2023-05-04 16:19:24 -0400

    Seeing if my code works so far

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,13 +2,24 @@ import os
 
 import csv
 
-csvpath = os.path.join("Resources","budget_data.csv")
+budget_csv = os.path.join("Resources","budget_data.csv")
 
 def Financial_Analysis(Budget_data):
-    Date = str(Budget_data[0])
-    Profit_Losses = int(Budget_data[1])
+    date = str(Budget_data[0])
+    profit_losses = int(Budget_data[1])
+
     
-with open(csvpath) as csvfile:
+    total_months = len(date)
+    total = sum(profit_losses)
+    average_change = (total / total_months)
+    
+
+
+
+   
+
+
+with open(budget_csv, 'r') as csvfile:
 
     csvreader = csv.reader(csvfile, delimiter=',')
 
@@ -17,9 +28,7 @@ with open(csvpath) as csvfile:
     csv_header = next(csvreader)
 
     Total_Months = len(list(csvreader))
-
-    def sum(numbers):
-        length = len(numbers)
-        Total = 0
+    print(Total_Months)
+   
     
-        Total = Total + (row["Profit/Losses"])
+        
