21fb61b5907d57346243b3570f17b7d148db1591
miemieanto-loJJAe <miemie.anto@gmail.com>
2023-05-04 23:21:06 -0400

    save

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,6 +7,10 @@ budget_data = os.path.join('Resources','budget_data.csv')
 #listing variables
 Total_Months = 0
 Total = 0
+Biggest_Increase = 0
+Biggest_Decrease = 0
+Highest_Month = ''
+Lowest_Month = ''
 
 with open(budget_data) as csvfile:
 
@@ -18,4 +22,11 @@ with open(budget_data) as csvfile:
     for row in csvreader:
         Total_Months += 1
         Total += int(row[1])
+        if int(row[1]) < Biggest_Decrease
+            Biggest_Decrease = int(row[1])
+            Lowest_Month = (row[0])
+        elif int(row[1]) > Biggest_Increase
+            Biggest_Increase = int(row[1])
+            Highest_Month = (row[0])
+    print(Biggest_Decrease)
 
