2bed27a833f17a20d48b2051f04e600909b7e474
miemieanto-loJJAe <miemie.anto@gmail.com>
2023-05-04 22:46:26 -0400

    save

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,10 +3,17 @@ import csv
 
 budget_data = os.path.join('Resources','budget_data.csv')
 
+#listing variables
+Total_Months = 0
+Total = 0
 
 with open(budget_data) as csvfile:
-    csvreader = csv.DictReader(csvfile)
+    csvreader = csv.reader(csvfile, delimiter=',')
+    csvheader = next(csvreader)
 
-    
+    print(csvreader)
+#going to create a loop
 
-  
+
+for row in csvreader:
+    Total_Months += 1
