91e3692b26c07606718b47993b78247e0d869310
miemieanto-loJJAe <miemie.anto@gmail.com>
2023-05-05 20:35:44 -0400

    set variables

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,4 +3,15 @@ import os
 #read csv file
 import csv
 
-election_data = os.path.join('')
+election_data = os.path.join('Resources','election_data.csv')
+
+txt_path = "Election_Analysis.txt"
+
+#declaring variables
+Total_Votes = 0
+Election_candidates = []
+Candidate_votes = {}
+Vote_Count = 0
+Election_winner = ""
+
+
